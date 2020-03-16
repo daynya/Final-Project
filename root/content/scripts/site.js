4532975af3d04dbb3960c88664f51bead3562251
Daynya Quigley <daynya@gmail.com>
2020-03-16 12:23:06 -0400

    added in basics for scroll to top - not done.

--- root/content/scripts/site.js
+++ root/content/scripts/site.js
@@ -1,3 +1,18 @@
 $(".menu-collapsed").click(function() {
     $(this).toggleClass("menu-expanded");
 });
+
+var scrollButton = $("#scrollBtn");
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() >300 ) {
+        scrollButton.addClass("show");
+    } else {
+        scrollButton.removeClass("show");
+    }
+});
+
+scrollButton.click(function(e) {
+    e.preventDefault();
+    $("html, body").animate({scrollTop:0}, '300');
+});
