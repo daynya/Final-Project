b2e50f90ecd3854f4fa313d80feb1d60b03986a6
Daynya Quigley <daynya@gmail.com>
2020-03-17 09:11:29 -0400

    Functional scroll to top, needs styled.

--- root/content/scripts/site.js
+++ root/content/scripts/site.js
@@ -2,17 +2,40 @@ $(".menu-collapsed").click(function() {
     $(this).toggleClass("menu-expanded");
 });
 
-var scrollButton = $("#scrollBtn");
+/*
+$(document).ready(function() {
+    var scrollTop = $("#scrollBtn");
 
-$(window).scroll(function () {
-    if ($(window).scrollTop() >300 ) {
-        scrollButton.addClass("show");
-    } else {
-        scrollButton.removeClass("show");
-    }
+    $(window).scroll(function() {
+        var top = $(this).scrollTop();
+
+        if (top > 100) {
+            $(scrollTop).css("opacity", "1");
+        } else {
+            $(scrollTop).css("opacity", "0");
+        }
+    });
+
+    $(scrollTop).click(function() {
+        $('html, body').animate({scrollTop: 0}, 800);
+        return false;
+    });
 });
+ */
 
-scrollButton.click(function(e) {
-    e.preventDefault();
-    $("html, body").animate({scrollTop:0}, '300');
+$(document).ready(function() {
+    var scrollButton = $("#scrollBtn");
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() >300 ) {
+            scrollButton.addClass("show");
+        } else {
+            scrollButton.removeClass("show");
+        }
+    });
+
+    scrollButton.click(function(e) {
+        e.preventDefault();
+        $("html, body").animate({scrollTop:0}, '300');
+    });
 });
