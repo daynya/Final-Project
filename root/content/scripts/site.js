7dd94cd5d29ee5f0f5c946f8cb57fdb9f5c4eced
Daynya Quigley <daynya@gmail.com>
2020-03-17 11:25:25 -0400

    styling of scroll to top

--- root/content/scripts/site.js
+++ root/content/scripts/site.js
@@ -2,27 +2,6 @@ $(".menu-collapsed").click(function() {
     $(this).toggleClass("menu-expanded");
 });
 
-/*
-$(document).ready(function() {
-    var scrollTop = $("#scrollBtn");
-
-    $(window).scroll(function() {
-        var top = $(this).scrollTop();
-
-        if (top > 100) {
-            $(scrollTop).css("opacity", "1");
-        } else {
-            $(scrollTop).css("opacity", "0");
-        }
-    });
-
-    $(scrollTop).click(function() {
-        $('html, body').animate({scrollTop: 0}, 800);
-        return false;
-    });
-});
- */
-
 $(document).ready(function() {
     var scrollButton = $("#scrollBtn");
 
